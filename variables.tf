1c338027d8688c1db34baa53ab54480a09cb96a6
peter <113434855+peters-code-hub@users.noreply.github.com>
2023-06-05 15:50:34 +0200

    typo fixes

--- variables.tf
+++ variables.tf
@@ -4,39 +4,38 @@ variable "instance_type" {
 }
 
 variable "ami_filter" {
-  description = "Name filter and owner of AMI
+  description = "Name filter and owner for AMI"
 
-  type = object ({
+  type    = object ({
     name  = string
     owner = string
   })
 
-  default {
-    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owners = "979382823631" # Bitnami
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
 variable "environment" {
-  description = "development environment"
+  description = "Deployment environment"
 
-  type = object ({
+  type        = object ({
     name           = string
     network_prefix = string
   })
-
-  default {
+  default = {
     name           = "dev"
     network_prefix = "10.0"
   }
 }
 
-variable "asg_min_size" {
-  description = "Minimum number of instances"
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
   default     = 1
 }
 
-variable "asg_max_size" {
-  description = "Maximum number of instances"
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
   default     = 2
 }
